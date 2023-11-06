34eff294f79a409feeece7891de66423a4ae6f90
David Adje <davidadjewiz9@gmail.com>
2023-11-06 15:45:50 +0000

    Creating new branch and new project

--- demo/src/test/java/com/luv2code/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/luv2code/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package com.luv2code.cruddemo;
+package com.luv2code.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CruddemoApplicationTests {
+class DemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
